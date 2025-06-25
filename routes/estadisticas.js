b47663d4a5e85136b475b918a793ad701bf29bed
Ian <al346169@edu.uaa.mx>
2025-06-25 00:19:12 -0600

    Era router con R mayúscula

--- routes/estadisticas.js
+++ routes/estadisticas.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const db = require("../models/conexion");
 
 router.get('/inscripciones-por-actividad', async (req, res) => {
