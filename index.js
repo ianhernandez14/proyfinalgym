b789bed5f64f1c2c04150ea88093444c4e78d262
Ian <al346169@edu.uaa.mx>
2025-06-24 23:58:15 -0600

    Nos faltó actualizar el index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const cors = require('cors');
 const { validationResult } = require('express-validator');
+const cors = require('cors');
 const rutas = require('./routes/rutas');
+const estadisticasRoutes = require('./routes/estadisticas');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//Middleware para manejar errores de validación
+// Middlewares
+const corsOptions = {
+  origin: 'https://proyfinalgym.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Validación
 const manejarErroresValidacion = (req, res, next) => {
   const errores = validationResult(req);
   if (!errores.isEmpty()) {
@@ -17,28 +27,18 @@ const manejarErroresValidacion = (req, res, next) => {
   }
   next();
 };
-
-//Middlewares
-const corsOptions = {
-  origin: 'https://proyfinalgym.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-};
-
-app.use(cors(corsOptions));
-app.use(express.json());
-
-//Aplicar middleware de validación globalmente
 app.use(manejarErroresValidacion);
 
-//Rutas
+// Rutas
 app.use('/api', rutas);
+app.use('/api/estadisticas', estadisticasRoutes);
 
-//Ruta de prueba
+// Ruta de prueba
 app.get('/', (req, res) => {
   res.send('API Firestore funcionando');
 });
 
-//Iniciar servidor
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
