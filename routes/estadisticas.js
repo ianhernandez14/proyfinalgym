9b7e583aa937c58c79ea53cbc2299e7e32f7cece
Ian <al346169@edu.uaa.mx>
2025-06-24 23:53:17 -0600

    Corrección V2

--- routes/estadisticas.js
+++ routes/estadisticas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./models/conexion');
+const db = require('../models/conexion');
 
 router.get('/inscripciones-por-actividad', async (req, res) => {
   try {
